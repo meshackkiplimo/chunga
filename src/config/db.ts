80a87486fabfd207282b21b6115cfadcf2b7a8e9
kimaiyo <meshackkimaiyo5@gmail.com>
2025-05-24 10:08:24 +0300

    feat: add database connection checks and update import paths

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,20 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { transactions } from "@/schema/transactionSchema";
-import env from "@/config/env";
+import env from "./env";
 
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, { schema: { transactions } });
+
+export const checkDatabaseConnection = () => {
+  pool.on("connect", () => {
+    console.log("Database is running! ✨");
+  });
+  
+  pool.on("error", (err) => {
+    console.error("Database connection error:", err);
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import transactionRoutes from "@/routes/transactionRoutes";
 import dotenv from "dotenv";
+import { db, checkDatabaseConnection } from "./config/db";
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api/transactions", transactionRoutes);
+
+// Check database connection
+checkDatabaseConnection();
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
